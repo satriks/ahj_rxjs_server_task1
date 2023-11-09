58861321ed7b44f33911995b841fd7e14adeff24
Yury Vereskun <satriks@mail.ru>
2023-11-10 02:50:14 +0300

    fix name

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Задание  ahj_webSocket_server_task2
+# Задание  ahj_rxjs_server_task1
 
 
--- routers/messages/index.js
+++ routers/messages/index.js
@@ -4,7 +4,11 @@ const router = new Router()
 
 router.get('/messages/unread', async (ctx) => {
 //   console.log(db.unread);
-
+  const broken = Math.random()
+  if (broken < 0.2) {
+    ctx.response.status = 500
+    return
+  }
   console.log('ping')
   ctx.response.body = {
     status: 'OK',
